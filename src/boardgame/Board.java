f61ea9094b4724a21eb375e2a1b8093e4b6820d6
Joubert Lucas <joubertvh1@hotmail.com>
2019-07-27 15:39:23 -0300

    criando tratamento de execoes

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,7 +1,5 @@
 package boardgame;
 
-import java.awt.Point;
-
 public class Board {
 
 	private int rows;
@@ -9,6 +7,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1) {
+			throw new BoardaException("erro ao criar tabuleiro: linha e columan 1");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -18,29 +19,44 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	
 	public Piece piece(int row, int column) {
+		if(!positionExists(row, column)) {
+			throw new BoardaException("Posicao nao esta no tabuleiro");
+		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardaException("Posicao nao esta no tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if(thereIsAPiece(position)) {
+			throw new BoardaException("ja existe peça na posicao "+  position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
 	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >=0 &&  column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardaException("ja existe peça na posicao "+  position);
+		}
+		return piece(position) != null;
+	}
 }
---
+++ src/boardgame/BoardaException.java
@@ -0,0 +1,11 @@
+package boardgame;
+
+public class BoardaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public BoardaException(String msg) {
+		super(msg);
+	}
+
+}
